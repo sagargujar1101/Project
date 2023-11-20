c4897e6507763650a56ee7f500c9ffe783c8e0bd
sagargujar1101 <127502010+sagargujar1101@users.noreply.github.com>
2023-11-20 22:29:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Header from "./Component/Header";
+// import Menu from "./Component/Menu";
+// import About from "./Component/About";
+// import Contact from "./Component/Contact";
+// import Location from "./Component/Location";
+// import HomePage from "./Component/File";
+// function App() {
+//   return (
+//    <>
+   
+//     <BrowserRouter>
+//       <Routes>
+      
+//         <Route path="/" element={<Header/>}/> 
+//         <Route path="menu" element={<Menu/>}/> 
+//         <Route path="location" element={<Location/>}/> 
+//         <Route path="about" element={<About/>}/> 
+//         <Route path="contact" element={<Contact/>}/> 
+        
+//       </Routes>
+//     </BrowserRouter>
+//     <HomePage/>
+//    </>
+//   );
+// }
+
+// export default App;
